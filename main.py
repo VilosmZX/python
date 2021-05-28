724c41eaa867c17ec3cc57baa348557e9d266fc5
VilosmZX <jravaell@gmail.com>
2021-05-28 15:28:43 +0700

    adding files.

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import json
+
+class main:
+
+    def __init__(self, name, pw):
+        self.name = name
+        self.pw = pw
+
+    def login(self):
+        name = input("Masukan nama: ").upper()
+        pw = input("Masukan pw: ").lower()
+        print(name)
+        if(name == self.name and pw == self.pw):
+            print("Login Sucess")
+        else:
+            print("Login gagal")
+
+a = main("joshua", "1234")
+main.login()
+
