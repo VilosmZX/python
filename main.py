51e84272508fdda1fb68efafd4f6e4c104314019
JoshuaRVL <70438760+VilosmZX@users.noreply.github.com>
2021-05-29 10:19:18 +0700

    update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import json
 
 class main:
 
-def get_quote():
-url = "https://zenquotes.io/api/random"
-res = requests.get(url)
-jsons = json.loads(res.text)
-quote = jsons[0]['q'] + "\n- " + jsons[0]['a']
-print(quote)
-
-if res = 404:
-print("url 404 not found")
-else :
-return
+  def get_quote():
+    url = "https://zenquotes.io/api/random"
+    res = requests.get(url)
+    jsons = json.loads(res.text)
+    quote = jsons[0]['q'] + "\n- " + jsons[0]['a']
+    print(quote)
+  
+    if res = 404:
+      print("url 404 not found")
+    else :
+      return
